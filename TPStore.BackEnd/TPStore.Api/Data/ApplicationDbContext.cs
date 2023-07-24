0492509c5a9e63d551f7a52504041d132ab1e0da
tan.nguyen.thanh <tan.nguyen.thanh@dmchosting.ca>
2023-07-24 17:16:54 +0700

    setup connection string and db context

---
+++ TPStore.BackEnd/TPStore.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TPStore.Api.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+            
+        }
+    }
+}
